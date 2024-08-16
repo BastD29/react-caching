adfca7d4610c00d7c8dfa7f2acfe77b9f9e14654
BastD29 <bastiendaniel9@gmail.com>
2024-08-16 15:54:44 +0200

    add store

--- src/pages/Store/Store.tsx
+++ src/pages/Store/Store.tsx
@@ -1,10 +1,30 @@
 import { FC } from "react";
+import useFetch from "../../hooks/useFetch";
+import LoaderLayout from "../../components/layouts/LoaderLayout/LoaderLayout";
 import style from "./Store.module.scss";
 
 const Store: FC = () => {
+  const { loading, error, data, refetch } = useFetch({
+    url: "https://randomuser.me/api",
+    method: "get",
+    key: ["app", "get", "user", { name: "nisab" }],
+    cache: {
+      enabled: true,
+      ttl: 10,
+    },
+  });
+
+  if (loading) {
+    return <LoaderLayout />;
+  }
+  if (error) {
+    return <p>Something went wrong</p>;
+  }
   return (
     <div className={style["store"]}>
-      <h2>Store</h2>
+      {JSON.stringify(data, null, 4)}
+      <br />
+      <button onClick={() => refetch()}>get user</button>
     </div>
   );
 };
